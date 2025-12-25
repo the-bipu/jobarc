2e3f0bb4b5ae9cfaa281f31bd3329bbd85681059
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-12-25 18:12:59 +0530

    Added New Components

---
+++ components/common/AdminTab.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AdminTab = () => {
+  return (
+    <div className='p-5 w-full h-full flex flex-col overflow-y-auto'>
+        <p className='text-base mb-3'>
+            Note: If we consider every scenario in the login and registration flow, only then can we build a fully user-friendly application. However, since this is a simple assessment, we’ve kept it straightforward and easy to understand, while still maintaining clean and high-quality code.
+        </p>
+        <p>
+            Here are few other sites that i've built which you may find interesting:
+        </p>
+        <p className='mt-2'>
+            1. <a href='https://www.internpluss.com/' target='_blank' rel='noopener noreferrer' className='hover:underline'>Internpluss</a>
+            <br />
+            2. <a href='https://slietalumni.in/' target='_blank' rel='noopener noreferrer' className='hover:underline'>SLIET Alumni Portal</a>
+            <br />
+            3. <a href='https://techfestsliet.com/' target='_blank' rel='noopener noreferrer' className='hover:underline'>techFEST'25</a>
+            <br />
+            4. <a href='https://techpratham.com/' target='_blank' rel='noopener noreferrer' className='hover:underline'>TechPratham</a>
+        </p>
+        <p className='font-medium mt-6'>
+            Thank you for taking the time to review my work. I hope you find the application meets your expectations and demonstrates my skills effectively.
+        </p>
+    </div>
+  )
+}
+
+export default AdminTab
